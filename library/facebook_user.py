8e2fafdfcca56406c4382504ee177a2fd70cdb5a
jasonwalsh <rightlag@gmail.com>
2018-10-04 20:29:50 -0400

    Add delete functionality

--- library/facebook_user.py
+++ library/facebook_user.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 """Ansible role for managing Facebook test users.
 """
+from __future__ import absolute_import
 
-from json import dumps
+from json import dumps, loads
 from urllib.error import HTTPError
 from urllib.request import Request, urlopen
 
@@ -47,6 +48,10 @@ def main():
                     'present'
                 ],
                 'default': 'present'
+            },
+            'test_user_id': {
+                'required': False,
+                'type': str
             }
         },
         supports_check_mode=True
@@ -55,6 +60,7 @@ def main():
     access_token = module.params['access_token']
     app_id = module.params['app_id']
     state = module.params['state']
+    test_user_id = module.params['test_user_id']
     method = {
         'absent': 'DELETE',
         'present': 'POST'
@@ -65,21 +71,25 @@ def main():
     }
     data = {}
     if method == 'POST':
+        path = '/{}/{}/accounts/test-users'.format(VERSION, app_id)
         data = {
             'permissions': ','.join(module.params['permissions'])
         }
+    else:
+        path = '/{}/{}'.format(VERSION, test_user_id)
     # Both DELETE and POST requests require access tokens.
     data['access_token'] = access_token
     # Encode the payload using the default character encoding.
     data = dumps(data).encode(DEFAULT_CHARACTER_ENCODING)
-    address = 'https://{}/{}/{}/accounts/test-users'.format(HOST, VERSION, app_id)
-    request = Request(address, data=data, headers=headers)
+    url = 'https://{}{}'.format(HOST, path)
+    request = Request(url, data=data, headers=headers, method=method)
     try:
         response = urlopen(request)
     except HTTPError as e:
         module.fail_json(msg=e.reason)
     changed = True
-    module.exit_json(changed=changed, status=response.status)
+    json = loads(response.read())
+    module.exit_json(changed=changed, status=response.status, json=json)
 
 
 if __name__ == '__main__':
